60d096aff7d2e7472538e33e35a2dd3b2fc4b469
Gandla Deepak <deepakgandla1@gmail.com>
2021-04-07 13:04:11 +0530

    tuple methods added

---
+++ tupleMethods.py
@@ -0,0 +1,9 @@
+fruits = ('mange', 'apple', 'cherry', 'apple')
+
+#count : Returns the number of times a specified value occurs in a tuple
+count = fruits.count('apple')
+print(count) #output 2
+
+#index : Searches the tuple for a specified value and returns the position of where it was found
+index = fruits.index('apple')
+print(index) #output 1
