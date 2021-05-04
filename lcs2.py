93757bcbcab40b82d5119e510bbb467f2b95a1f6
Gandla Deepak <deepakgandla1@gmail.com>
2021-05-04 19:02:00 +0530

    palindrome

---
+++ lcs2.py
@@ -0,0 +1,4 @@
+from difflib import SequenceMatcher
+s ='aaaa'
+s1 = 'aab'
+s = SequenceMatcher(None, s, s1)
---
+++ removecharacter.py
@@ -0,0 +1,10 @@
+def remove(st, ch):
+	new_st = st.replace(ch, "")
+	return new_st
+print(remove('weclome all', 'l'))
+a = 'abac'
+b = 'ca'
+if b in a:
+	print('yes')
+n = 2
+print(f'{n:.3f}')
